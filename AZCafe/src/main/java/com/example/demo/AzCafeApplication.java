4904191eeec75b5ebfc982d6d829d7f1d4702b25
西野直幸 <nishino@asojuku.ac.jp>
2020-09-15 18:33:59 +0900

    new:空のプロジェクトをアップ

---
+++ AZCafe/src/main/java/com/example/demo/AzCafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzCafeApplication.class, args);
+	}
+
+}
