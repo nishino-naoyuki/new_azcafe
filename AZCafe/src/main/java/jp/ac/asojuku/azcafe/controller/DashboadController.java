2156c5ff72919a9ce79f63090633f906a377a4ed
西野直幸 <nishino@asojuku.ac.jp>
2020-12-26 18:06:59 +0900

    モックアップ作成中

--- AZCafe/src/main/java/jp/ac/asojuku/azcafe/controller/DashboadController.java
+++ AZCafe/src/main/java/jp/ac/asojuku/azcafe/controller/DashboadController.java
@@ -3,10 +3,20 @@
  */
 package jp.ac.asojuku.azcafe.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * @author nishino
  * ダッシュボード画面
  */
 public class DashboadController {
 
+	@RequestMapping(value= {"/","/dashboad"}, method=RequestMethod.GET)
+    public ModelAndView login(ModelAndView mv) {
+		
+		mv.setViewName("login");
+		return mv;
+	}
 }
